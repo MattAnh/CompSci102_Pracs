05243cc3e0fedc6e46fcdb43a326f919df26faed
Selinah <g17m4640@campus.ru.ac.za>
2018-07-27 11:43:41 +0200

    calculating change (how many notes/coins) and taxes.

---
+++ Prac02_Classwork/ChangeCalculator/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an amount: ");
+            string amount = Console.ReadLine();
+
+            double theAmount;
+
+            //input validation
+            if (Double.TryParse(amount, out theAmount))
+            {                                               //THE COMMENTS APPLY FOR THE WHOLE CODE
+                double remainder1 = theAmount % 200;        //calculating the remainder from the user input
+                double note200 = theAmount - remainder1;    //finding the exact multiple of the note/coin by subtracting remainder from initial amount
+                double count1 = note200 / 200;              //using count to find how many of a certain note/coin there is in the given amount
+                if (count1 > 0)                             //checking if the certain note/coin was found
+                {
+                    Console.WriteLine("Return " + count1 +  " x R200 note(s).");
+                }
+
+                double remainder2 = remainder1 % 100;
+                double note100 = remainder1 - remainder2;
+                double count2 = note100 / 100;
+                if (count2 > 0)
+                {
+                    Console.WriteLine("Return " + count2 + " x R100 note(s).");
+                }
+
+                double remainder3 = remainder2 % 50;
+                double note50 = remainder2 - remainder3;
+                double count3 = note50 / 50;
+                if (count3 > 0)
+                {
+                    Console.WriteLine("Return " + count3 + " x R50 note(s).");
+                }
+
+                double remainder4 = remainder3 % 20;
+                double note20 = remainder3 - remainder4;
+                double count4 = note20 / 20;
+                if (count4 > 0)
+                {
+                    Console.WriteLine("Return " + count4 + " x R20 note(s).");
+                }
+
+                double remainder5 = remainder4 % 10;
+                double note10 = remainder4 - remainder5;
+                double count5 = note10 / 10;
+                if (count5 > 0)
+                {
+                    Console.WriteLine("Return " + count5 + " x R10 note(s).");
+                }
+
+                double remainder6 = remainder5 % 5;
+                double coin1 = remainder5 - remainder6;
+                double count6 = coin1 / 5;
+                if (count6 > 0)
+                {
+                    Console.WriteLine("Return " + count6 + " x R5 coin(s).");
+                }
+
+                double remainder7 = remainder6 % 2;
+                double coin2 = remainder6 - remainder7;
+                double count7 = coin2 / 2;
+                if (count7 > 0)
+                {
+                    Console.WriteLine("Return " + count7 + " x R2 coin(s).");
+                }
+
+                double remainder8 = remainder7 % 1;
+                double coin3 = remainder7 - remainder8;
+                double count8 = coin3 / 1;
+                if (count8 > 0)
+                {
+                    Console.WriteLine("Return " + count8 + " x R1 coin(s).");
+                }
+
+                double remainder9 = remainder8 % 0.5;
+                double coin4 = remainder8 - remainder9;
+                double count9 = coin4 / 0.5;
+                if (count9 > 0)
+                {
+                    Console.WriteLine("Return " + count9 + " x 50c coin(s).");
+                }
+
+                double remainder10 = remainder9 % 0.2;
+                double coin5 = remainder9 - remainder10;
+                double count10 = coin5 / 0.2;
+                if (count10 > 0)
+                {
+                    Console.WriteLine("Return " + count10 + " x 20c coin(s).");
+                }
+
+                double remainder11 = remainder10 % 0.1;
+                double coin6 = remainder10 - remainder11;
+                double count11 = coin6 / 0.1;
+                if (count11 > 0)
+                {
+                    Console.WriteLine("Return " + count11 + " x 10c coin(s).");
+                }
+
+                double remainder12 = remainder11 % 0.05;
+                double coin7 = remainder11 - remainder12;
+                double count12 = coin7 / 0.05;
+                if (count12 > 0)
+                {
+                    Console.WriteLine("Return " + count12 + " x 5c coin(s).");
+                }
+
+                double remainder13 = remainder12 % 0.02;
+                double coin8 = remainder12 - remainder13;
+                double count13 = coin8 / 0.02;
+                if (count13 > 0)
+                {
+                    Console.WriteLine("Return " + count13 + " x 2c coin(s).");
+                }
+
+                double remainder14 = remainder13 % 0.01;        //this part of the code does not run for numbers greater than 1000
+                double coin9 = remainder13 - remainder14;
+                double count14 = coin9 / 0.01;
+                if (count14 > 0)
+                {
+                    Console.WriteLine("Return " + count14 + " x 1c coin(s).");
+                }
+
+            }
+            else
+            {
+               Console.WriteLine("You entered an invalid amount! Please try again.");
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Prac02_Classwork/TaxesCalculator/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxesCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your monthly salary: ");
+            int salary = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enetr your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (0 <= salary && salary <= 4999)
+            {
+                double payableTax = 0;
+                Console.WriteLine("Your monthly tax is " + payableTax);
+            }
+            else if (5000 <= salary && salary <= 6499)
+            {
+                if (age < 65)
+                {
+                    double payableTax = 13;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+            }
+            else if (6500 <= salary && salary <= 7799)
+            {
+                if (age < 65)
+                {
+                    double payableTax = 250;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+            }
+            else if (7800 <= salary && salary <= 9999)
+            {
+                if (age < 65)
+                {
+                    double payableTax = 620;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+            }
+            else if (10000 <= salary && salary <= 13499)
+            {
+                if (age < 65)
+                {
+                    double payableTax = 1200;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+                else if (65 <= age && age <= 75)
+                {
+                    double payableTax = 600;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+                else if (age > 75)
+                {
+                    double payableTax = 400;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+            }
+            else if (13500 <= salary && salary <= 17899)
+            {
+                if (age < 65)
+                {
+                    double payableTax = 2100;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+                else if (65 <= age && age <= 75)
+                {
+                    double payableTax = 1500;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+                else if (age > 75)
+                {
+                    double payableTax = 1200;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+            }
+            else if (17900 <= salary && salary <= 19999)
+            {
+                if (age < 65)
+                {
+                    double payableTax = 2700;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+                else if (65 <= age && age <= 75)
+                {
+                    double payableTax = 2000;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+                else if (age > 75)
+                {
+                    double payableTax = 1800;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+            }
+            else if (20000 <= salary && salary <= 39999)
+            {
+                if (age < 65)
+                {
+                    double payableTax = 8000;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+                else if (65 <= age && age <= 75)
+                {
+                    double payableTax = 7500;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+                else if (age > 75)
+                {
+                    double payableTax = 7300;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+            }
+            else if (40000 <= salary && salary <= 59999)
+            {
+                if (age < 65)
+                {
+                    double payableTax = 16000;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+                else if (65 <= age && age <= 75)
+                {
+                    double payableTax = 15600;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+                else if (age > 75)
+                {
+                    double payableTax = 15400;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+            }
+            else
+            {
+                if (age < 65)
+                {
+                    double payableTax = 20000;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+                else if (65 <= age && age <= 75)
+                {
+                    double payableTax = 17050;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+                else if (age > 75)
+                {
+                    double payableTax = 17000;
+                    Console.WriteLine("Your monthly tax is " + payableTax);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
